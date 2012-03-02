f0303ddae49fd13d78713b32a987da7b656cff15
mehan jayasuriya <mehan.j@gmail.com>
2012-03-02 15:18:26 -0500

    changed models.js

--- models.js
+++ models.js
@@ -5,8 +5,10 @@ module.exports.configureSchema = function(Schema, mongoose) {
     Memes = new Schema({
       line1 : String,
         line2 : String,
-        image : String
-    });
+        image : String,
+        memeNumber : Number,
+        
+        });
     
     // add schemas to Mongoose
     mongoose.model('Meme', Memes);

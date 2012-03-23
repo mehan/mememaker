4012c95830b73fdd91e8495b4a87adff589750a5
mehan jayasuriya <mehan.j@gmail.com>
2012-03-23 11:39:17 -0400

    added this time

--- web.js
+++ web.js
@@ -129,6 +129,95 @@ app.get('/meme/:urlslug', function(request, response){
 	
 });
 
+// return all blog entries in json format
+app.get('/data/allmemes', function(request, response){
+
+    // define the fields you want to include in your json data
+    includeFields = ['line1','line2','image','urlslug']
+
+    // query for all blog
+    queryConditions = {}; //empty conditions - return everything
+    var query = Meme.find( queryConditions, includeFields);
+
+    query.sort('date',-1); //sort by most recent
+    query.exec(function (err, Memes) {
+
+        // render the card_form template with the data above
+        jsonData = {
+          'status' : 'OK',
+          'memes' : Memes
+        }
+
+        response.json(jsonData);
+    });
+});
+
+//edit and update post
+
+app.get('/meme/:urlslug/edit', function(request, response){
+
+  var templateData = { 
+        pageTitle : 'EDIT YR MEME',
+        message: 'EDIT	 YR MEME',
+        images: memeImages
+    };
+    
+	Meme.findOne({urlslug:request.params.urlslug},function(err, memeData){
+	
+		if (err) {
+		   console.log('error');
+		   console.log(err);
+		   response.send("Sorry, yr meme was not found!");
+		}
+			
+		if (memeData.urlslug != undefined) {
+			
+			// Render the card_display template - pass in the cardData
+			response.render("meme_edit.html", memeData); } 
+		
+		else {
+			// card not found. show the 'Card not found' template
+			response.render("meme_not_found.html");}
+		
+	});
+	
+});
+
+
+app.post('/meme/:urlslug/update', function(request, response){
+
+var requestedSlug = request.params.urlslug;
+var condition = { urlslug : requestedSlug };
+
+   var updatedData = {
+        line1 : request.body.line1,
+        line2 : request.body.line2,
+        image : request.body.image,
+        urlslug: newSlug,
+    }; 
+  
+   var options = { multi : false };
+   
+   Meme.update( condition, updatedData, options, function(err, numAffected){
+
+        if (err) {
+            console.log('Update Error Occurred');
+            response.send('Update Error Occurred ' + err);
+
+        } else {
+
+            console.log("update succeeded");
+            console.log(numAffected + " document(s) updated");
+
+            //redirect the user to the update page - append ?update=true to URL
+            response.redirect('/update/' + postid + "?update=true");
+
+        }
+    });
+
+});
+
+
 
 // Make server turn on and listen at defined PORT (or port 3000 if is not defined)
 var port = process.env.PORT || 3000;
